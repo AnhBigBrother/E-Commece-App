d368918714984f02553e8e30a904b597c96ff508
BigBruhh <anh.bigbrother@gmail.com>
2024-05-05 21:23:20 +0700

    deploy server to render.com

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   withCredentials: true,
-  baseURL: 'https://e-commece-api.vercel.app/api',
+  baseURL: 'https://e-commece-czc4.onrender.com/api',
   headers: {
     // withCredentials: true,
     'Content-Type': 'application/json',
