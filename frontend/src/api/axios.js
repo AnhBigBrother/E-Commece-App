db037bade3c79d25770871168463f6cb76987467
BigBruhh <anh.bigbrother@gmail.com>
2024-05-05 21:27:16 +0700

    deploy server to render.com

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default axios.create({
   withCredentials: true,
-  baseURL: 'https://e-commece-czc4.onrender.com/api',
+  baseURL: 'https://bruhh-e-commece-server.onrender.com/api',
   headers: {
     // withCredentials: true,
     'Content-Type': 'application/json',
