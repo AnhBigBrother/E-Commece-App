9f17df4938ddb54b71b67b203cc1435966af4cbd
BigBruhh <anh.bigbrother@gmail.com>
2024-05-05 16:08:21 +0700

    fixed: upload and delete product

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,7 +2,6 @@ import axios from 'axios';
 
 export default axios.create({
   withCredentials: true,
-  timeout: 10000,
   baseURL: 'https://e-commece-api.vercel.app/api',
   headers: {
     // withCredentials: true,
--- frontend/src/pages/Admin/ManageProducts.jsx
+++ frontend/src/pages/Admin/ManageProducts.jsx
@@ -8,6 +8,7 @@ import { Skeleton4, Skeleton4Item } from '../../components/Skeleton.jsx';
 
 const ProductItem = ({ data }) => {
   const navigate = useNavigate();
+  const [visible, setVisible] = useState(true);
   const [isLoading, setIsLoading] = useState(false);
   const handleDeleteProduct = () => {
     if (isLoading) {
@@ -20,41 +21,44 @@ const ProductItem = ({ data }) => {
         .then(res => {
           console.log(res);
           setIsLoading(false);
-          window.location.reload();
           toast.success('Product deleted successfuly');
+          setVisible(false);
         })
         .catch(err => {
           console.error(err);
           setIsLoading(false);
-          window.location.reload();
           toast.error('Something wrong, try later');
         });
     }
   };
   return (
-    <div className='flex flex-row gap-3 bg-neutral-200 dark:bg-neutral-800 rounded-md p-3'>
-      <img
-        className='rounded-md h-40 aspect-square object-cover cursor-pointer'
-        src={data.imageUrl}
-        onClick={() => navigate(`/products/${data._id}`)}></img>
-      <div className='flex flex-col gap-2 flex-grow'>
-        <p className='text-lg font-semibold text-ellipsis line-clamp-1'>{data.name}</p>
-        <p className='text-ellipsis line-clamp-2 h-[3rem]'>{data.description}</p>
-        <p className='text-xl font-semibold'>&#36;&#32;{data.price}</p>
-        <div className='flex flex-row gap-3 justify-start'>
-          <button
-            className='px-2 py-1 bg-rose-500 hover:bg-rose-700 rounded-md'
-            onClick={() => navigate(`${data._id}`)}>
-            Update
-          </button>
-          <button
-            className='px-2 py-1 bg-rose-500 hover:bg-rose-700 rounded-md'
-            onClick={() => handleDeleteProduct()}>
-            {isLoading ? <ButtonLoader /> : <span>Delete</span>}
-          </button>
+    <>
+      {visible && (
+        <div className='flex flex-row gap-3 bg-neutral-200 dark:bg-neutral-800 rounded-md p-3'>
+          <img
+            className='rounded-md h-40 aspect-square object-cover cursor-pointer'
+            src={data.imageUrl}
+            onClick={() => navigate(`/products/${data._id}`)}></img>
+          <div className='flex flex-col gap-2 flex-grow'>
+            <p className='text-lg font-semibold text-ellipsis line-clamp-1'>{data.name}</p>
+            <p className='text-ellipsis line-clamp-2 h-[3rem]'>{data.description}</p>
+            <p className='text-xl font-semibold'>&#36;&#32;{data.price}</p>
+            <div className='flex flex-row gap-3 justify-start'>
+              <button
+                className='px-2 py-1 bg-rose-500 hover:bg-rose-700 rounded-md'
+                onClick={() => navigate(`${data._id}`)}>
+                Update
+              </button>
+              <button
+                className='px-2 py-1 bg-rose-500 hover:bg-rose-700 rounded-md'
+                onClick={() => handleDeleteProduct()}>
+                {isLoading ? <ButtonLoader /> : <span>Delete</span>}
+              </button>
+            </div>
+          </div>
         </div>
-      </div>
-    </div>
+      )}
+    </>
   );
 };
 
