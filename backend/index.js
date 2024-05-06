1b951ac13ef8399128857ba12bf873e16a47094f
BigBruhh <anh.bigbrother@gmail.com>
2024-05-06 13:40:27 +0700

    deploy server to render.com

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ const port = process.env.PORT;
 const app = express();
 
 // cors
-const whitelist = ['http://localhost:5173', 'https://e-commece-bigbruhh.vercel.app'];
+const whitelist = ['http://localhost:5173', 'https://e-commece-bigbruhh.vercel.app', 'https://bruhh-e-commece.onrender.com'];
 const corsOptions = {
   credentials: true,
   origin: function (origin, callback) {
